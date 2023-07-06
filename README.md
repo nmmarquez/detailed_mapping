40539dc07eb95344e4132757494534efd5540614
Neal Marquez <nmmarquez@gmail.com>
2023-07-06 12:44:13 -0400

    mapping update

--- README.md
+++ README.md
@@ -11,3 +11,9 @@ Map of Seattle tracts taken directly from census APIs.
 Removed water features and added roads for context.
 
 ![](images/updated.png)
+
+# With inlet for spatial zoom
+
+Add a new sub map which has a zoomed in version of downtown with added context like road names.
+
+![](images/final.png)
--- mapping_census_vars.R
+++ mapping_census_vars.R
@@ -1,15 +1,19 @@
 rm(list = ls())
-library(tidyverse)
-library(sf)
-library(tidycensus)
-library(tigris)
-library(maptiles)
-library(tidyterra)
+library(tidyverse) # data manipulation
+library(sf) # spatial data objects
+library(tidycensus) # get census data easily
+library(tigris) # get spatial data easily
+library(maptiles) # tiles for adding context to plots
+library(tidyterra) # easy plotting of map tiles
+library(cowplot) # combine multiple plots together
+library(ggspatial) # plot spatial objects like bounding boxes
 
+# pull Seattle spatial object
 seattle <- places("WA", year = 2018, class = "sf") %>%
     filter(NAME == "Seattle") %>%
     st_transform(32148)
 
+# pull tract level mhi data
 mhi_raw_sf <- get_acs(
     geography = "tract", variables = "B19013_001", state = "WA",
     county = "King", geometry = TRUE) %>%
@@ -24,7 +28,8 @@ mhi_raw_sf <- get_acs(
         labels = c(
             "20-40k", "40-75k", "75-100k", "100-125k", "125-150k", ">150k")))
 
-mhi_raw_sf %>%
+# very basic r plot
+basic_map <- mhi_raw_sf %>%
     ggplot() +
     geom_sf(aes(fill = MHIQ)) +
     scale_fill_ordinal(na.value = "grey50") +
@@ -34,29 +39,83 @@ mhi_raw_sf %>%
         "Seattle Median Houshold Income",
         "MHI by Census Tract")
 
+# remove water
 mhi_sf <- mhi_raw_sf %>%
     erase_water(area_threshold = 0.9)
 
+# get map tiles adding road context
 dc <- mhi_sf %>%
     get_tiles(
         provider = "Stamen.TonerLines",
-        zoom = 11, crop = T)
-
-ggplot() +
-    geom_spatraster_rgb(data = dc)
+        zoom = 12, crop = T)
 
 road_map <- ggplot() +
-    geom_spatraster_rgb(data = dc) +
-    geom_sf(
-        data = mhi_sf,
-        aes(fill=MHIQ),
-        alpha = .4) +
+    geom_spatraster_rgb(data = dc) + # plot road map tiles
+    geom_sf(data = mhi_sf, aes(fill=MHIQ), alpha = .4) +
     coord_sf(crs = 32148) +
-    scale_fill_ordinal(na.value = "grey50") +
+    scale_fill_ordinal(na.translate=FALSE) + # remove "na" from legend
     theme_void(base_size = 14) +
     labs(fill="Median\nHousehold\nIncome") +
-    ggtitle(
-        "Seattle Median Houshold Income",
-        "MHI by Census Tract")
+    theme(legend.position = c(1.13,.37), legend.background=element_blank())
 
-road_map
+# what are the geographies we want to focus on
+keep_geos <- str_c(
+    "5303300",
+    c(
+        "7201", "7202", "7302", "8002", "8003", "8101", "8102",
+        "8200", "8300", "8401", "8402", "8500","9200"))
+
+# cropped version of main spatial object focusing on downtown geographies
+cropped_mhi_sf <- mhi_sf %>%
+    st_crop(st_bbox(filter(mhi_sf, GEOID %in% keep_geos)))
+
+# get the map tiles for downtown this time with road names
+dc2 <- cropped_mhi_sf %>%
+    get_tiles(
+        provider = "Stamen.TonerHybrid",
+        zoom = 14, crop = T)
+
+# make zoomed in plot
+inlet_map <- cropped_mhi_sf %>%
+    ggplot() +
+    geom_spatraster_rgb(data = dc2) +
+    geom_sf(data = cropped_mhi_sf, aes(fill=MHIQ), alpha = .4) +
+    coord_sf(crs = 32148) +
+    scale_fill_ordinal(na.value = "grey50", na.translate=FALSE) +
+    theme_void(base_size = 14) +
+    theme(legend.position = "bottom") +
+    layer_spatial(
+        data=st_bbox(filter(mhi_sf, GEOID %in% keep_geos)),
+        alpha = 0,
+        size=1.15,
+        color = "red") +
+    labs(fill="Median\nHousehold\nIncome")
+
+# make the final plot using r package "cowplot"
+final_map <- ggdraw(
+    road_map +
+        theme(legend.position = "none") +
+        layer_spatial(
+            data=st_bbox(filter(mhi_sf, GEOID %in% keep_geos)),
+            alpha = 0,
+            size=1.15,
+            color = "red")
+        , xlim = c(0,.7)) +
+    draw_plot(
+        inlet_map+theme(legend.position = "none"),
+        width = .36,
+        height = .36,
+        x=-.01,
+        y=.27) +
+    draw_plot(
+        get_legend(inlet_map),
+        width = .36,
+        height = .36,
+        x=.01,
+        y=.01
+    )
+
+
+ggsave("./images/final.png", final_map, width = 9, height = 10)
+
+
